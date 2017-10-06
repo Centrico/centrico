098756f9fa783923a7d1cf53c138e54ffe3ccf26
GERMAN CABEZAS <GERMAN.CABEZAS@LIVE.COM>
2017-10-06 15:05:26 -0400

    adds markdown files as pages through gastby api

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,20 +6,28 @@ var ReactMarkdown = require('react-markdown');
 
 
 var mark = `
-# Contents
+ Contents
 `;
 
 export default ({ data }) => {
   return (
     <div>
       <ReactMarkdown source={mark} />
+      <h1>pages filtered by folder</h1>
       {data.allSitePage.edges.map(({ node }) =>
         <div>
           <Link to={node.path}>
             {node.path.toString()}
           </Link>
         </div>
-
+      )}
+      <h1> Markdown </h1>
+      {data.allMarkdownRemark.edges.map(({ node }) =>
+        <div key={node.id}>
+            <h3>{node.frontmatter.title}{" "}</h3>
+            {node.frontmatter.date}
+            {node.excerpt}
+        </div>
       )}
     </div>
   )
@@ -28,13 +36,27 @@ export default ({ data }) => {
 export const query = graphql`
   query MyFilesQuery {
     allSitePage
-     (filter: {path: {regex: "/projects/i"}})
+     (filter: {path: {regex: "/posts/i"}})
      {
        edges {
          node {
            path
          }
        }
+    }
+
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
     }
   }
 `
