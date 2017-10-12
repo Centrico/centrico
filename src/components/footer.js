562536f7f4921116a4df108e4a9bd54a51a6ecea
GERMAN CABEZAS <GERMAN.CABEZAS@LIVE.COM>
2017-10-11 22:00:36 -0500

    footer styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { Menu, Grid, Container, Segment } from 'semantic-ui-react'
+import { Menu, Grid, Container, Segment, Icon } from 'semantic-ui-react'
 import Link from 'gatsby-link'
-import logo from '../../public/images/centrico-logo-300(2x).png'
+import logo from '../../public/images/centrico-logo-300(2x)-negative.png'
 
 export default class MenuExampleBasic extends Component {
   state = {}
@@ -14,31 +14,51 @@ export default class MenuExampleBasic extends Component {
     return (
       <div className={"footer"}>
         <Container>
+          <Segment padded={"very"} basic className={"site-logo"}>
+            <Link to="/">
+              <img src={logo} alt="Logo" />
+            </Link>
+          </Segment>
           <Segment padded={"very"} basic className={"fluid-grid"}>
             <Grid columns={3}>
               <Grid.Column>
-              STRATEGY
-              UX-design
-              Product Management
-              Project Management
+              <p>STRATEGY</p>
+              <Link to="/services/"> UX Design</Link>
+              <Link to="/services/"> Product Management</Link>
+              <Link to="/services/"> Project Management</Link>
               </Grid.Column>
               <Grid.Column>
-              DESIGN
-              Interface Design
-              Brand Identity
-              Web Design
-              Graphic Design
-              Advertising
+              <p>DESIGN</p>
+              <Link to="/services/"> Interface Design</Link>
+              <Link to="/services/"> Brand Identity</Link>
+              <Link to="/services/"> Web Design</Link>
+              <Link to="/services/"> Graphic Design</Link>
+              <Link to="/services/"> Advertising</Link>
               </Grid.Column>
               <Grid.Column>
-              DEVELOPMENT
-              Custom Solutions
-              Native Apps
-              E-Commerce
-              Content Management
+              <p>DEVELOPMENT</p>
+              <Link to="/services/">Custom Solutions</Link>
+              <Link to="/services/"> Native Apps</Link>
+              <Link to="/services/"> E-Commerce</Link>
+              <Link to="/services/">Content Management</Link>
               </Grid.Column>
             </Grid>
           </Segment>
+          <Grid columns={3}>
+            <Grid.Column>
+              <Segment padded={'very'} basic>
+                <Icon link name='linkedin' size={'large'} />
+                <Icon link name='facebook f' size={'large'} />
+                <Icon link name='twitter' size={'large'} />
+                <Icon link name='google plus' size={'large'} />
+              </Segment>
+            </Grid.Column>
+            <Grid.Column>
+              <Segment padded={'very'} basic>
+                Copyright Centrico LLC 2017
+              </Segment>
+            </Grid.Column>
+          </Grid>
         </Container>
       </div>
     )
