76775e9f6b9ed7e97c83ee385e46b4b608fbf739
GERMAN CABEZAS <GERMAN.CABEZAS@LIVE.COM>
2017-11-06 08:58:15 -0600

    adds content to about

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { Grid, Container, Image, Header, Input, Form, Button, Segment, Label } from 'semantic-ui-react'
+import ContactForm from '../components/form'
+
 
 const Contact = () => (
   <div>
-    <h1>Contact</h1>
+    <Container>
+      <Segment padded={"very"} basic>
+        <Header as={'h2'} color={"purple"} className={"main-header"}>Start a Project with Us!</Header>
+        <p>We’ve been producing top quality products for over a decade combined of efforts that give CENTRICO a unique approach. Our experience working in corporate, startup and consulting setups give us the confidence to understand what you mean, what you want and the urgency of the matter. We strive to provide you the best we can be with any project.</p>
+      </Segment>
+    </Container>
+    <div className={"form"}>
+      <Container>
+        <Segment padded={"very"} basic className={"fluid-grid"}>
+          <Header as={'h2'}>Like what you see?  LETS TALK.</Header>
+          <Header as={'h4'} className={"spaced"}>SUBMIT REQUEST</Header>
+          <ContactForm/>
+        </Segment>
+      </Container>
+    </div>
   </div>
 )
 
