8eb44b794b9fb9b2238726ae42e9de885ea3e093
German Cabezas <gcabezas@Germans-MacBook-Pro.local>
2020-11-29 22:57:01 -0500

    Updates

---
+++ component---src-pages-404-js-a4efff70d2262a2a6317.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(t,e,n){"use strict";n.r(e),function(t){n("q1tI");e.default=function(){return t.createElement("div",null,t.createElement("h1",null,"NOT FOUND"),t.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}.call(this,n("iMUK"))}}]);
+//# sourceMappingURL=component---src-pages-404-js-a4efff70d2262a2a6317.js.map
